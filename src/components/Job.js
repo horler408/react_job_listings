fdb4811510cbf05e5cb15d5b9827d36a0618ab15
horler408 <horler.web@gmail.com>
2020-10-16 10:15:58 +0100

    Initial Bootstrap styling

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import {Card} from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
+import {ReactMarkdown} from 'react-markdown';
 
 export default function Job({job}) {
     return (
         <Card>
             <Card.Body>
-                {job.title}
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <Card.Title>
+                            {job.title} - <span className="text-muted font-size-light">{job.company}</span>
+                        </Card.Title>
+                        <Card.Subtitle className="text-muted mb-2">
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+                        <Badge variant="secondary">{job.location}</Badge>
+                        <div style={{wordBreak: "break-all"}}>
+                            <ReactMarkdown source={job.how_to_apply} />
+                        </div>
+                    </div>
+                </div>
             </Card.Body>
             
         </Card>
